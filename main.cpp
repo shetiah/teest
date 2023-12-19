a38ff331af63a83c3b1a7aa1aa7d10811b875fd9
basemyousef <basenyousef@gmail.com>
2023-12-19 02:08:38 +0200

    here is div dum3 master

--- main.cpp
+++ main.cpp
@@ -1,14 +1,15 @@
 #include<iostream>
 using namespace std;
-int sum()
+int sum3()
 {
-    return 2+2;
+    return 8+2;
 }
+int div();
 int main()
 {
     int s=99;
     int ja=0;
     int iLove=00;
-    cout << sum();
+    cout << sum3();
     return 0;
 }

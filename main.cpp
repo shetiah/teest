c09bb0c4ee58d3bf4b00c7e9f6432e9731c7eaa2
basemyousef <basenyousef@gmail.com>
2023-12-19 02:30:33 +0200

    rebased again

--- main.cpp
+++ main.cpp
@@ -5,8 +5,19 @@ int sum3()
     return 8+2;
 }
 int sas();
+int mult();
 int main()
 {
+    for (size_t i = 0; i < 5; i++)
+    {
+        /* code */
+        for (size_t i = 0; i < 10; i++)
+        {
+            /* code */
+        }
+        
+    }
+    
     int s=99;
     int ja=0;
     int iLove=00;
